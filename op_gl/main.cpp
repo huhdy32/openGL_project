c69447e81a15be522610e1094a361aacb99dcc4d
huhdy32 <mong3268@gmail.com>
2023-04-02 13:34:37 +0900

    First Ex

--- op_gl/main.cpp
+++ op_gl/main.cpp
@@ -4,11 +4,27 @@
 //
 //  Created by 윤희종 on 2023/04/01.
 //
+#include <OpenGL/gl.h>
+#include <OpenGl/glu.h>
+#include <GLUT/glut.h>
 
-#include <iostream>
+void Mydisplay() {
+    glClear(GL_COLOR_BUFFER_BIT);
+    glBegin(GL_POLYGON);
+    glVertex3f(-0.5, -0.5, 0.0);
+    glVertex3f(-0.5, 0.5, 0.0);
+    glVertex3f(0.5, 0.5, 0.0);
+    glVertex3f(0.5, -0.5, 0.0);
+    glEnd();
+    glFlush();
+}
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main(int argc, char** argv) {
+    glutInit(&argc, argv);
+    glutInitWindowSize(400, 400);
+    glutCreateWindow("My OpenGL Window");
+    glutDisplayFunc(Mydisplay);
+    glutMainLoop();
     return 0;
 }
+
